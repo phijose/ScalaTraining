6833e81cede9b7db6642649bbb90e37f2d67966c
Phijose J Philip <phijosewiz@gmail.com>
2024-10-09 10:56:13 +0530

    completed task 3 implement conditional statements

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,21 @@ package com.tigmaminds.scala
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val a = 30
+    println("a = "+a)
+    val b = if (a > 9) 70 else 0
+    println("b = "+b)
+    val c = 9
+    println("c = "+c)
+    val greater = if (a > b) "a" else if (b > a) "b" else "c"
+    println({
+      greater match {
+        case "a" => "a is greater"
+        case "b" => "b is greater"
+        case "c" => "c is greater"
+        case _ => "something bad happened"
+      }
+    })
+
   }
 }
