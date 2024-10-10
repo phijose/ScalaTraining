b1c391fc7c0551fd4884aa04095df544026bfc3d
Phijose J Philip <phijosewiz@gmail.com>
2024-10-10 11:03:38 +0530

    completed task 4 implement loop statement and functions

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Scala Training
+
+## Result
+
+```
+Loops and Functions
+
+Stars
+
+*
+
+*  *
+
+*  *  * 
+
+*  *  *  * 
+
+Even
+2
+4
+6
+8
+10
+
+Do While
+4. Hi its a Do While!
+3. Hi its a Do While!
+2. Hi its a Do While!
+1. Hi its a Do While!
+
+Odd Even Combination :
+(1,2) (1,4) (1,6) (1,8) (1,10) (3,2) (3,4) (3,6) (3,8) (3,10) (5,2) (5,4) (5,6) (5,8) (5,10) (7,2) (7,4) (7,6) (7,8) (7,10) (9,2) (9,4) (9,6) (9,8) (9,10)
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,49 @@
 package com.tigmaminds.scala
 
 object Main {
+
+  def star(n: Int) = {
+    var tem = 1
+    while(tem<n) {
+      for(i <- 1 to tem)
+        print(" * ")
+      println()
+      tem+=1
+    }
+  }
+
+  def doWhile(): Unit = {
+    var a = 4
+    do{
+      println(a+". Hi its a Do While!")
+      a-=1
+    }while(a>0)
+  }
+
+  def evenAndOdd(a: Int=10): Unit = {
+    println("\nOdd Even Combination : ")
+    for(i <- 1 to a by(2); j <- 2 to a by(2))
+      print(s"($i,$j) ")
+  }
+
+  val even = (n: Int) => for(i <- 2 to n by(2)) println(i)
+
+  var prompt = "Loops and Functions"
+
+  def simpleExp: Unit = println("\n"+this.prompt)
+
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    simpleExp
+    this.prompt="Stars"
+    simpleExp
+    star(5)
+    this.prompt="Even"
+    simpleExp
+    even(10)
+    this.prompt="Do While"
+    simpleExp
+    doWhile()
+    evenAndOdd()
   }
 }
