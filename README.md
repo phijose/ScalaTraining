bdb77ddd9e276b2797896f91cc6047a90bfd8518
Phijose J Philip <phijosewiz@gmail.com>
2024-10-15 12:01:26 +0530

    completed task 6 Higher Order Functions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Scala Training
+
+## Task 6
+```text
+Higher Order functions
+
+sum of 1 to 10 with 100 = 155
+function return function = 20
+Binary function as argument Sorting = List(Apple, Banana, Guava, Orange)
+function as Argument = List(Apple-5, Orange-6, Guava-5, Banana-6)
+Currying function = Hi!, Bineesh
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,26 @@
 package com.tigmaminds.scala
 
+import scala.collection.mutable
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Higher Order functions")
+//    var fabList = mutable.Seq[Int]()
+    val sum = (1 to 10).toList.fold(100)(_ + _)
+    println("\nsum of 1 to 10 with 100 = "+sum)
+    val tenMultiplier = multiplier(10)
+    println("function return function = "+tenMultiplier(2))
+    val fruits = List("Apple", "Orange", "Guava", "Banana")
+    println("Binary function as argument Sorting = "+fruits.sortWith(_ < _))
+    println("function as Argument = "+listModifier(fruits,(x:String) => x.length))
+    val greeter = appendStringInFront("Hi!, ")_
+    println("Currying function = " + greeter("Bineesh"))
+
   }
+  private def multiplier(x: Int) =
+    (a :Int) => a * x
+  private def listModifier(arr: List[String], any: String => Int) =
+    arr.map(x => x+"-"+any(x))
+  private def appendStringInFront(a:String)(b:String) =
+    a + b
 }
