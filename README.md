0087d7562cceb51003b6bd79a6b9b07e19bc7cf2
Phijose J Philip <phijosewiz@gmail.com>
2024-10-16 12:53:37 +0530

    completed task 7 Immutable Collections and Operations

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Scala Training
+
+## Task 7
+```text
+Immutable Collections and Operations
+
+fruits.groupBy(_.length) = HashMap(5 -> List(apple, guava, peach), 6 -> List(orange, banana))
+fruits.map(_.capitalize) = List(Apple, Orange, Banana, Guava, Peach)
+fruits.foldRight('') = apple -> orange -> banana -> guava -> peach
+fruits.sorted = List(apple, banana, guava, orange, peach)
+fruits.sortWith(_ > _) = List(peach, orange, guava, banana, apple)
+fruits.take(2) = List(apple, orange)
+fruits.drop(2) = List(banana, guava, peach)
+fruitLen.zip(fruits) = List((5,apple), (6,orange), (6,banana), (5,guava), (5,peach))
+fruits.partition = (List(orange, banana),List(apple, guava, peach))
+flatmap = List(1, 2, 2, 4, 3, 6)
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,19 @@ package com.tigmaminds.scala
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Immutable Collections and Operations\n")
+    val fruits = List("apple","orange","banana","guava","peach")
+    println("fruits.groupBy(_.length) = "+fruits.groupBy(_.length))
+    println("fruits.map(_.capitalize) = "+fruits.map(_.capitalize))
+    println("fruits.foldRight('') = "+fruits.foldRight("")
+    ((a:String,b:String) => if(b!="") a +" -> "+ b else a))
+    println("fruits.sorted = "+fruits.sorted)
+    println("fruits.sortWith(_ > _) = "+fruits.sortWith(_ > _))
+    println("fruits.take(2) = "+fruits.take(2))
+    println("fruits.drop(2) = "+fruits.drop(2))
+    val fruitLen = fruits.map(_.length)
+    println("fruitLen.zip(fruits) = "+fruitLen.zip(fruits))
+    println("fruits.partition = "+fruits.partition(_.length % 2 == 0))
+    println("flatmap = "+(1 to 3).toList.flatMap(x => List(x,x*2)))
   }
 }
