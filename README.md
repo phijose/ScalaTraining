e3576df70b25c73a12dbe279edbd16c745e39d5a
Phijose J Philip <phijosewiz@gmail.com>
2024-10-17 11:42:36 +0530

    completed task 8 Tuples and option

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Scala Training
+
+## Task 8
+```text
+Tuples and Option Types
+
+vegies = (1,Cabbage,44.25,true)
+SNo.1 Cabbage 44.25rs Paid
+evenProvider(2) = Some(2)
+evenProvider(1) = None
+tuple match = (1,2,3)
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,19 @@ package com.tigmaminds.scala
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Tuples and Option Types\n")
+    val vegies = (1,"Cabbage",44.25,true)
+    println("vegies = "+vegies)
+    println(s"SNo.${vegies._1} ${vegies._2} ${vegies._3}rs ${if (vegies._4) "Paid" else "Not Paid"}")
+    println("evenProvider(2) = "+evenProvider(2))
+    println("evenProvider(1) = "+evenProvider(1))
+    val tuple = (1, 2, 3)
+    val transformed = tuple match {
+      case (a, b, c) => (a , b , c )
+    }
+    println("tuple match = "+ transformed)  // (2, 4, 6)
+  }
+  def evenProvider(a: Int): Option[Int] = {
+    if (a%2==0) Some(a) else None
   }
 }
