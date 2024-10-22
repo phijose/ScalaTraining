5e54ac63341377a2a3221ea50c159afaa94892fe
Phijose J Philip <phijosewiz@gmail.com>
2024-10-22 15:02:06 +0530

    completed task 11 class and object

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Scala Training
+
+## Task 11
+```text
+Classes and Objects
+
+1. Classes in Scala
+Hi, Phijose you are 24 years old
+2. Objects in Scala
+Have a Good Day Phijose
+3. Companion Object
+Hi, Joel you are 22 years old
+4. Case Classes
+Car(BMW,M2 Coupe,4)
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,31 @@ package com.tigmaminds.scala
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Classes and Objects\n")
+    println("1. Classes in Scala")
+    val aPerson = new Person("Phijose", 24)
+    println(aPerson)
+    println("2. Objects in Scala")
+    println(PersonUtils.greet(aPerson))
+    println("3. Companion Object")
+    val anotherPerson = Person("Joel",22)
+    println(anotherPerson)
+    println("4. Case Classes")
+    val acar = Car("BMW","M2 Coupe",4)
+    println(acar)
   }
 }
+
+class Person(val name: String,val age: Int = 25){
+  override def toString: String = s"Hi, ${this.name} you are ${this.age} years old"
+}
+
+object PersonUtils {
+  def greet(person: Person) = s"Have a Good Day ${person.name}"
+}
+
+object Person{
+  def apply(name: String, age: Int): Person = new Person(name, age)
+}
+
+case class Car(brand:String, name: String, noOfPassengers: Int)
