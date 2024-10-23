524e3f6543ca0e360fa7657056098dd01051414e
Phijose J Philip <phijosewiz@gmail.com>
2024-10-23 15:28:43 +0530

    completed task 12 inheritance and traits

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Scala Training
+
+## Task 12
+```text
+Inheritance and Traits
+
+1. Basic Inheritance
+Hello from Child
+2. Constructor Inheritance
+Parent Constructor: John
+Child Constructor: John, 30
+3. Trait basic usage
+Hello from Trait
+4. Using Multiple Traits
+Walking...
+Talking...
+5. Traits with Abstract Members
+Woof
+6. Combining Inheritance and Traits
+Flying...
+Swimming...
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,64 @@ package com.tigmaminds.scala
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Inheritance and Traits\n")
+    println("1. Basic Inheritance")
+    class Parent {
+      def greet: () => Unit = () => println("Hello from Parent")
+    }
+    class Child extends Parent {
+      override def greet: () => Unit = ()  => println("Hello from Child")
+    }
+    val child = new Child()
+    child.greet()
+
+    println("2. Constructor Inheritance")
+    class Parent1(name: String) {
+      println(s"Parent Constructor: $name")
+    }
+    class Child1(name: String, age: Int) extends Parent1(name) {
+      println(s"Child Constructor: $name, $age")
+    }
+    val child1 = new Child1("John", 30)
+
+    println("3. Trait basic usage")
+    trait Greetable {
+      def greet(): Unit = println("Hello from Trait")
+    }
+    class Person extends Greetable
+    val person = new Person()
+    person.greet()
+    println("4. Using Multiple Traits")
+    trait Walkable {
+      def walk(): Unit = println("Walking...")
+    }
+    trait Talkable {
+      def talk(): Unit = println("Talking...")
+    }
+    class Human extends Walkable with Talkable
+    val human = new Human()
+    human.walk()
+    human.talk()
+    println("5. Traits with Abstract Members")
+    trait Animal {
+      def sound(): String
+    }
+    class Dog extends Animal {
+      override def sound(): String = "Woof"
+    }
+    val dog = new Dog()
+    println(dog.sound())
+    println("6. Combining Inheritance and Traits")
+    trait Flyer {
+      def fly(): Unit = println("Flying...")
+    }
+    trait Swimmer {
+      def swim(): Unit = println("Swimming...")
+    }
+    class Bird extends Flyer
+    class Duck extends Bird with Swimmer
+    val duck = new Duck()
+    duck.fly()
+    duck.swim()
   }
 }
