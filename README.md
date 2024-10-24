bdd34cdb0c9b71b4bd25561b314fce3280a8294c
Phijose J Philip <phijosewiz@gmail.com>
2024-10-24 14:57:19 +0530

    completed task 13 Case Classes and Companion Objects

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Scala Training
+
+## Task 13
+```text
+Case Classes and Companion Objects
+
+1. case class
+Object equality = true
+Create Copy of object with update only required = Point(1,3)
+case class matching = Point at (1, 2)
+2. Using object with case class
+Area: 15.0
+Perimeter: 16.0
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,28 @@
 package com.tigmaminds.scala
 
+case class Point(x: Int, y: Int)
+
+case class Rectangle(length: Double, width: Double)
+
+object Rectangle {
+  def area(rect: Rectangle): Double = rect.length * rect.width
+  def perimeter(rect: Rectangle): Double = 2 * (rect.length + rect.width)
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Case Classes and Companion Objects\n\n1. case class")
+    val point1 = Point(1, 2)
+    val point2 = Point(1, 2)
+    println("Object equality = "+(point1 == point2))
+    val point3 = point1.copy(y = 3)
+    println("Create Copy of object with update only required = "+point3)
+    point1 match {
+      case Point(x, y) => println(s"case class matching = Point at ($x, $y)")  // Output: Point at (1, 2)
+    }
+    val rect1 = Rectangle(5.0, 3.0)
+    println("2. Using object with case class")
+    println(s"Area: ${Rectangle.area(rect1)}")
+    println(s"Perimeter: ${Rectangle.perimeter(rect1)}")
   }
 }
