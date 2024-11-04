c1605fdb946939a6e584657ef18849008fbb468c
Phijose J Philip <phijosewiz@gmail.com>
2024-11-04 18:06:37 +0530

    completed task 19 Introduction to functional programing - Pure functions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Scala Training
+
+## Task 19
+
+```text
+Introduction to functional programing
+ Same input, Same Value, Always
+5
+5
+Array(2, 4, 6)
+Array(2, 4, 6)
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,19 @@ package com.tigmaminds.scala
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("File I/O and Working with JSON")
+    println("Introduction to functional programing\n Same input, Same Value, Always")
+    println(add(2, 3))
+    println(add(2, 3))
+    val numbers = Array(1, 2, 3, 4, 5, 6)
+    println(getEvenNumbers(numbers).mkString("Array(", ", ", ")"));
+    println(getEvenNumbers(numbers).mkString("Array(", ", ", ")"));
+  }
+
+  def getEvenNumbers(numbers: Array[Int]): Array[Int] = {
+    numbers.filter(_ % 2 == 0);
+  }
+
+  def add(a :Int, b: Int): Int = {
+    a + b
   }
 }
